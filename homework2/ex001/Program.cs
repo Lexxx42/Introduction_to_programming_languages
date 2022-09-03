5153cb21947164bf6aba3ade13c37b8ace5fffa9
Lexxx42 <111595809+Lexxx42@users.noreply.github.com>
2022-09-03 17:29:24 +0300

    2 exercises

---
+++ homework2/ex001/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int SearchForSecondNumber(int X)
+{
+    int p = 0;
+    X = X/10;
+    if(X > 0)
+    {
+        p = X%10;
+    }
+    else
+    {
+        p = -X%10;
+    }
+    return p;
+}
+
+int Prompt(string message)
+{
+    System.Console.Write(message);        // Вывод приглашения
+    string strValue;                      // Объявление переменной для ввода строки
+    strValue = Console.ReadLine() ?? "0"; // Вводим строку с консоли (с консоли можно ввести только строку)
+    int value = int.Parse(strValue);      // Преобразование строки в целое число
+    return value;
+}
+
+System.Console.WriteLine("This program takes a three-digit number as input and outputs the second digit of that number.");
+int value = Prompt("Please, enter some three-digit number > ");
+
+
+if((Math.Abs(value)>99 & Math.Abs(value)<1000))
+{
+System.Console.WriteLine($"2nd digit is {SearchForSecondNumber(value)}");
+}
+else
+{
+    System.Console.WriteLine("Your number have to be a three-digit number!");
+}
---
+++ homework2/ex002/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+void ThreeDigitNumberWithoutMiddle(int X)
+{
+    int result = -1;
+    if(X==0) {result = new Random().Next(100,1000);}
+    if(X==1) {result = new Random().Next(-1000,-100);}
+    System.Console.WriteLine($"Our random number is > {result}");
+    int last_digit = result;
+    int first_digit = result;
+    if(result > 0)
+    {
+        last_digit = result%10;
+        while (first_digit > 10) { first_digit /= 10; }
+    }
+    else
+    {
+        last_digit = -result%10;
+        while (first_digit < -10) { first_digit /= 10; }
+    }
+    System.Console.WriteLine($"Number without middle digit is {first_digit}{last_digit}");
+}
+System.Console.WriteLine("This program prints a random three-digit number and removes the second digit of this number");
+ThreeDigitNumberWithoutMiddle(new Random().Next(0,2));
+
