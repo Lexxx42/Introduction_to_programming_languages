2c7ebf78947f24fc98a9f98547843df2ad9239e8
Lexxx42 <111595809+Lexxx42@users.noreply.github.com>
2022-09-03 13:19:47 +0300

    hello.py

--- other/ex001_online_practicum/hello.py
+++ other/ex001_online_practicum/hello.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+from ssl import ALERT_DESCRIPTION_ACCESS_DENIED
+
+
+a = 5
+b = 3
+print("a =", a,"b =", b)
+c = a * b
+print('a + c =', a + c)
+# Comment!
+if (a > b) :
+    print('a > b')
+else:
+    print('a < b')
