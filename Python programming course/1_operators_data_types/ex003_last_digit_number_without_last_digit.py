7462d83adc6395683fe0a6716aa2dd1f94c87af1
Alexander Konukhov <jesusmisha@gmail.com>
2022-09-20 21:48:27 +0300

    python_course

---
+++ Python programming course/1_operators_data_types/ex003_last_digit_number_without_last_digit.py
@@ -0,0 +1,12 @@
+number = 2014
+
+# Last digit.
+last_digit = number % 10
+
+# Number without last digit.
+number_without_last_digit = number // 10
+
+# Number in power 2.
+number_in_pow = number ** 14
+
+print(number_in_pow)
---
+++ Python programming course/1_operators_data_types/ex004_binary_unary_constructions.py
@@ -0,0 +1,16 @@
+# sum, pow, *, / - binary operands
+# существуют и унарные операторы, которые принимают 1 аргумент:
+# взятие числа с противоположным знаком -42
+
+number = 42
+
+negative_number = -number
+
+# оператор оставления знака у числа
+number_without_symbol = +number
+
+a = +-+42 
+# a = -42
+
+# incorrect construction.
+# -*42
