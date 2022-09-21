6c39ce37f890a96bcf56604ab294ed99bfe902b1
Alexander Konukhov <jesusmisha@gmail.com>
2022-09-21 22:00:38 +0300

    lection5

---
+++ lecture5/ex001/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (9,7)"
+                .Replace("(", "")
+                .Replace(")", "")
+                ;
+
+///
+var data = text.Split(" ")
+                .Select(item => item.Split(','))
+                .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))
+                .Where(e => e.x % 2 == 0)
+                .Select(point => (point.x * 10, point.y))
+                .ToArray();
+for (int i = 0; i < data.Length; i++)
+{
+    System.Console.WriteLine(data[i]);
+}
+
