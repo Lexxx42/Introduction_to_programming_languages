97fa99dd929ce95964883f9010e0368886c9ae7b
Alexander Konukhov <jesusmisha@gmail.com>
2022-09-22 21:57:15 +0300

    1.8 course

---
+++ Python programming course/1_operators_data_types/ex009_var_pract.py
@@ -0,0 +1,16 @@
+a = 3
+print(a)
+a+=4
+print(a)
+print(a*2)
+print(a, a*2)
+
+# name = input('what is your name? ')
+# print("hello,",name)
+
+a = int(input("Enter a number a "))
+
+b = int(input("Enter a number b "))
+
+print(a*b)
+
