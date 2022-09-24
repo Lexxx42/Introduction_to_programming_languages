047438b73a2fb7e634cb9ee75df4031c2b468c89
Alexander Konukhov <jesusmisha@gmail.com>
2022-09-24 10:03:04 +0300

    seminar8

---
+++ seminar8/ex001/Program.cs
@@ -0,0 +1,19 @@
+﻿// string b = string.Empty; - works
+
+
+// show random numbers in random position
+
+/* Random r = new Random();
+
+System.Console.CursorVisible = false;
+while(true)
+{
+    Console.SetCursorPosition(
+        left: r.Next(Console.WindowWidth),
+        top: r.Next(Console.WindowHeight)
+    );
+    System.Console.Write(r.Next(10));
+    Thread.Sleep(1000);
+} */
+
+
