e7905ac6ce8cf7594d54955568239a7f81debcba
Alexander Konukhov <jesusmisha@gmail.com>
2022-09-25 21:21:33 +0300

    test

---
+++ test_projectVS/helloWorld/ConsoleVS/ConsoleVS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using ListMaster;
+
+
+namespace ConsoleVS
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            ArrayMaster am = new();
+            for (int i = 0; i < 10; i++)
+            {
+                am.Add(new Random().Next(1,5));
+            }
+            for (int i = 0; i < am.GetCount(); i++)
+            {
+                Console.WriteLine(am.GetValue(i));
+            }
+        }
+    }
+}
