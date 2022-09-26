ceadc2f1b6da1e65a28dfdb296f6b57aabddfd41
Alexander Konukhov <jesusmisha@gmail.com>
2022-09-26 22:46:31 +0300

    1.11

---
+++ Python programming course/1_operators_data_types/ex024_hw9.py
@@ -0,0 +1,11 @@
+# Отметьте выражения, значения которых равны True:
+
+print("239" < "30" and 239 < 30) # false
+print()
+print("239" < "30" and 239 > 30) # true
+print()
+print("239" > "30" and 239 < 30) # false
+print()
+print("239" > "30" and 239 > 30) # false
+
+# https://stepic.org/media/attachments/course67/week_1.html#%D0%A1%D1%82%D1%80%D0%BE%D0%BA%D0%B8
---
+++ Python programming course/1_operators_data_types/ex025_strings.py
@@ -0,0 +1,11 @@
+
+a = 'string'
+b = 'another string'
+print(a,b)
+print()
+print(a+b)
+print()
+print(a)
+print(b)
+print()
+print(a+'\n'+b)
