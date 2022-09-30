077a757ac5ebe53c03aa2f12fc4dbc360798bf1d
Alexander Konukhov <jesusmisha@gmail.com>
2022-09-30 21:36:26 +0300

    calculator

--- Python programming course/1_operators_data_types/ex029_simple_calsulator.py
+++ Python programming course/1_operators_data_types/ex029_simple_calsulator.py
@@ -12,3 +12,63 @@
 
 # Обратите внимание, что на вход программе приходят вещественные числа.
 
+def Call(operation_code, number_1, number_2):
+    if operation_code == '+':
+        result(summarize(number_1, number_2))
+    elif operation_code == '-':
+        result(diff(number_1, number_2))
+    elif operation_code == '/':
+        if number_2 == 0:
+            print('Деление на 0!')
+        else:
+            result(divide(number_1, number_2))
+    elif operation_code == '*':
+        result(multiply(number_1, number_2))
+    elif operation_code == "mod":
+        if number_2 == 0:
+            print('Деление на 0!')
+        else:
+            result(ostatok(number_1, number_2))
+    elif operation_code == "pow":
+        result(power(number_1, number_2))
+    elif operation_code == "div":
+        if number_2 == 0:
+            print('Деление на 0!')
+        else:
+            result(div(number_1, number_2))
+
+
+def summarize(num1, num2):
+    return num1+num2
+
+
+def diff(num1, num2):
+    return num1-num2
+
+
+def divide(num1, num2):
+    return num1/num2
+
+
+def multiply(num1, num2):
+    return num1*num2
+
+
+def ostatok(num1, num2):
+    return num1 % num2
+
+
+def power(num1, num2):
+    return num1 ** num2
+
+
+def div(num1, num2):
+    return num1//num2
+
+def result(number):
+    print(number)
+
+first_number = float(input())
+second_number = float(input())
+operation = input()
+Call(operation, first_number, second_number)
