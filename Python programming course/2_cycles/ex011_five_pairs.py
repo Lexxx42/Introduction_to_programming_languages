6479cda141cdf8d1e803a8a53225b3ce791103d5
Alexander Konukhov <jesusmisha@gmail.com>
2022-10-17 20:28:15 +0300

    ex011

---
+++ Python programming course/2_cycles/ex011_five_pairs.py
@@ -0,0 +1,45 @@
+# читаем 5 пар чисел и выводим их произведение
+
+# i = 0
+# while i < 5:
+#     a, b = input().split(',')
+#     a = int(a)
+#     b = int(b)
+#     print(a*b)
+#     i += 1
+
+
+# прерывание цикла break
+
+# i = 0
+# while i < 2:
+#     a, b = input().split(',')
+#     a = int(a)
+#     b = int(b)
+#     if a == 0 and b == 0:
+#         print("break of 1")
+#         break  # досрочно прерываем цикл
+#     print(a*b)
+#     i += 1
+# else: - условие если цикл завершился не по оператору break
+# else:
+#     print("end of 1")
+
+
+# continue - оператор перехода к следующей итерации цикла (если она есть)
+
+i = 0
+while i < 5:
+    a, b = input().split(',')
+    a = int(a)
+    b = int(b)
+    if a == 0 and b == 0:
+        print(i)
+        break  # досрочно прерываем цикл
+    if a == 0 or b == 0:
+        continue # переходим к след итерации
+    print(a*b)
+    i += 1
+# else: - условие если цикл завершился не по оператору break
+else:
+    print("end of 2")
