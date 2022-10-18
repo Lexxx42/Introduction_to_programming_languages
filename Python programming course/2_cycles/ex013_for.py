c0ba96de735d476372a83a25224f7f4706ed6cf8
Alexander Konukhov <jesusmisha@gmail.com>
2022-10-18 19:52:12 +0300

    ex013

---
+++ Python programming course/2_cycles/ex013_for.py
@@ -0,0 +1,17 @@
+# все элементы среди цифр
+
+for i in 2, 3, 5:
+    print(i*i)
+
+print()
+
+for i in range(1,11,2):
+    print(i*i)
+
+# exercise: cube output
+
+size = 4
+for i in range(1,size+1):
+    print('*'*size)
+
+
