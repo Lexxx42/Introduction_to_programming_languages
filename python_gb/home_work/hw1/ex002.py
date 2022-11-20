5dae345cab0c19e220def886c82b45b46bbb5758
Alexander Konukhov <jesusmisha@gmail.com>
2022-11-20 23:14:32 +0300

    upd ex002.py hw

--- python_gb/home_work/hw1/ex002.py
+++ python_gb/home_work/hw1/ex002.py
@@ -2,18 +2,21 @@
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
 def check_for_falsity(x, y, z):
-    if (x or y or z) != (not x and not y and not z):
-        print('Statement is True')
-    else:
-        print('Statement is False')
+    print('X', 'Y', 'Z', 'L', 'R', 'S', sep='\t')
+    for i in range(len(x)):
+        left = x[i] or y[i] or z[i]
+        right = not x[i] and not y[i] and not z[i]
+        s = left != right
+        print(f'{(x[i])}', f'{(y[i])}', f'{(z[i])}', left, int(right), int(s), sep='\t')
 
 
-def user_input():
-    x = input('Please enter X value > ')
-    y = input('Please enter Y value > ')
-    z = input('Please enter Z value > ')
+def value_input():
+    x = [0, 0, 0, 0, 1, 1, 1, 1]
+    y = [0, 0, 1, 1, 0, 0, 1, 1]
+    z = [0, 1, 0, 1, 0, 1, 0, 1]
     check_for_falsity(x, y, z)
 
 
 print("This program checks if the ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z assertion is false for all values of the predicate")
-user_input()
+print('L: (¬X ⋀ ¬Y ⋀ ¬Z)', 'R: (¬X ⋀ ¬Y ⋀ ¬Z)', 'S: (X ⋁ Y ⋁ Z) != (¬X ⋀ ¬Y ⋀ ¬Z)', '\n')
+value_input()
