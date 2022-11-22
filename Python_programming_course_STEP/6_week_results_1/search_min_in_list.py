612240bea189cb437880a54162972dd9af30e88e
Alexander Konukhov <jesusmisha@gmail.com>
2022-11-22 22:03:57 +0300

    hw

--- Python_programming_course_STEP/6_week_results_1/search_min_in_list.py
+++ Python_programming_course_STEP/6_week_results_1/search_min_in_list.py
@@ -1,9 +1,24 @@
 # search for min in the list
 
-a = [5, 8, 4, 3, 5, 7]
-
+#a = list(int(i) for i in input().split())
+a = [int(i) for i in input().split()]
 min_a = a[0]
 for i, item in enumerate(a):
     if item < min_a:
         min_a = item
 print(min_a)
+print()
+# simpler
+min_a = a[0]
+for c in a:
+    if c < min_a:
+        min_a = c
+print(min_a)
+print()
+# simpler
+a.sort()
+print(a[0])
+print()
+# simpler
+print(min(a))
+print()
