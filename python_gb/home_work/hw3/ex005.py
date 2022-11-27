4c61c7c34a34ce432bf6a95314d01ba02daa813d
Alexander Konukhov <jesusmisha@gmail.com>
2022-11-27 17:34:20 +0300

    some code entered

--- python_gb/home_work/hw3/ex005.py
+++ python_gb/home_work/hw3/ex005.py
@@ -6,10 +6,16 @@
 # out
 # -21 13 -8 5 -3 2 -1 1 0 1 1 2 3 5 8 13 21
 
-def neg_fib()
-
-
+def neg_fib(num):
+    fib_list = [0, 1]
+    for i in range(1, num):
+        fib_list.append(fib_list[i] + fib_list[i - 1])
+    print(fib_list)
+    for i in range(0, -n, -1):
+        fib_list.insert(0, fib_list[i - 1] + fib_list[i - 2])
+    print(fib_list)
 
 
 print('This program generates a list of Fibonacci numbers, including those for negative indexes.')
-
+n = int(input('Enter a Fibonacci number: '))
+neg_fib(n)
