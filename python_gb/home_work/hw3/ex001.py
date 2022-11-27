4612429cf2482d508457bc35685d5bf4b20455a5
Alexander Konukhov <jesusmisha@gmail.com>
2022-11-27 13:51:32 +0300

    hw GB ex001.py UPD

--- python_gb/home_work/hw3/ex001.py
+++ python_gb/home_work/hw3/ex001.py
@@ -1,16 +1,34 @@
-# Задайте список из нескольких чисел.
-# Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
-# Пример:
-# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+# 1. Задайте список, состоящий из произвольных чисел, количество задаёт пользователь.
+# Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётных позициях(не индексах).
+# in
+# 5
+#
+# out
+# [10, 2, 3, 8, 9]
+# 22
+#
+# in
+# 4
+#
+# out
+# [4, 2, 4, 9]
+# 8
 
-def sum_list_elements(input_list):
+def input_data_in_list(length):
+    output_list = []
+    for i in range(length):
+        output_list.append(int(input(f"Enter list's {i + 1} value: ")))
+    print(output_list)
+    return output_list
+
+
+def sum_of_elements_in_list(list_for_sum):
     sum_elements = 0
-    for i in range(0, len(input_list), 2):
-        sum_elements += input_list[i]
+    for i in range(0, len(list_for_sum), 2):
+        sum_elements += list_for_sum[i]
     return sum_elements
 
 
 print('This program finds the sum of the elements of a list in an odd position.')
-list_1 = [2, 3, 5, 9, 3]
-print(f'List for sum of the elements calculation is {list_1}')
-print(f'Sum of the elements of the list = {sum_list_elements(list_1)}')
+list_length = int(input('Enter the length of the list: '))
+print(f'Sum of the elements in list = {sum_of_elements_in_list(input_data_in_list(list_length))}')
