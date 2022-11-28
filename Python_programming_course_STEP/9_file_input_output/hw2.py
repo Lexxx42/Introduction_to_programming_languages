4aa65976320a3e49f608a12451043ed3935558a4
Alexander Konukhov <jesusmisha@gmail.com>
2022-11-28 19:58:24 +0300

    hw2

--- Python_programming_course_STEP/9_file_input_output/hw2.py
+++ Python_programming_course_STEP/9_file_input_output/hw2.py
@@ -1,9 +1,21 @@
-with open('input_data.txt') as inf:  # закрывается самостоятельно
+with open('dataset_3363_3.txt') as inf:  # закрывается самостоятельно
     text = ''
     for line in inf:
         line = line.strip()
         text += line + ' '
     text = text.lower()
-    list_1 = list(text)
-    print()
-
+    list_1 = text.split(' ')
+    dict_answer = {list_1[0]: 0}
+    for i in range(len(list_1)):
+        dict_answer[list_1[i]] = 0
+    for i in range(len(list_1)):
+        dict_answer[list_1[i]] = dict_answer.get(list_1[i]) + 1
+    key_max = ''
+    val_max = 0
+    for key, value in dict_answer.items():
+        if value > val_max:
+            key_max = key
+            val_max = value
+        else:
+            continue
+    print(key_max, val_max)
