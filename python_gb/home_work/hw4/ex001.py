6b29c6d7eece8d5048e0bc577293ea5ad20802f0
Alexander Konukhov <jesusmisha@gmail.com>
2022-12-03 16:16:16 +0300

    ex001.py

--- python_gb/home_work/hw4/ex001.py
+++ python_gb/home_work/hw4/ex001.py
@@ -1,3 +1,28 @@
 # Вычислить число c заданной точностью d
 # Пример:
 # - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+from decimal import Decimal, ROUND_HALF_UP
+
+
+def round_decimal(number: Decimal, rounding: Decimal):
+    output = Decimal(number.quantize(Decimal(str(rounding)[1:]), rounding=ROUND_HALF_UP))
+    print(output)
+    return output
+
+
+def user_input():
+    real_number = Decimal(input('Enter a real number: '))
+    accuracy = Decimal(input("Enter the required accuracy '0.0001': "))
+    if 1 <= accuracy or accuracy.is_signed():
+        print('Accuracy must be positive and lower than 1.')
+        user_input()
+    return real_number, accuracy
+
+
+def main():
+    real_number, accuracy = user_input()
+    round_decimal(real_number, accuracy)
+
+
+if __name__ == '__main__':
+    main()
