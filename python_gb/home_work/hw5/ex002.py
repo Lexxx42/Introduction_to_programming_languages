a610a61e6ac98e1f07a886e5542f6a8f3207e4e2
Alexander Konukhov <jesusmisha@gmail.com>
2022-12-04 14:08:55 +0300

    ex001.py done

---
+++ python_gb/home_work/hw5/ex002.py
@@ -0,0 +1,14 @@
+# 2. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.Входные и выходные данные хранятся в отдельных текстовых файлах.
+# Алгоритм RLE
+#
+# in
+# Enter the name of the file with the text:
+# 'text_words.txt'
+# Enter the file name to record:
+# 'text_code_words.txt'
+# Enter the name of the file to decode:
+# 'text_code_words.txt'
+#
+# out
+# aaaaavvvvvvvvvvvvvvvvvvvvvvvvvvvvvssssDDDdddFFggggOOiiiaa
+# vvvvvvvvvvvbbwwPPuuuTTYyWWQQ
