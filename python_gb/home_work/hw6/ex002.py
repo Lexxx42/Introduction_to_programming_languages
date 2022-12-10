9109cf089dab999bb973baa3309fa8d3888efcee
Alexander Konukhov <jesusmisha@gmail.com>
2022-12-10 16:20:42 +0300

    ex002.py done

--- python_gb/home_work/hw6/ex002.py
+++ python_gb/home_work/hw6/ex002.py
@@ -1,3 +1,4 @@
+""" This program for numbers between 20 and N finds multiples of 20 or 21. """
 # 2. Для чисел в пределах от 20 до N найти числа, кратные 20 или 21. Use comprehension.
 # in
 # 100
@@ -13,3 +14,7 @@
 # 140, 147, 160, 168, 180, 189, 200, 210, 220, 231, 240,
 # 252, 260, 273, 280, 294, 300, 315, 320, 336, 340, 357,
 # 360, 378, 380, 399, 400, 420]
+
+n = int(input())
+list_input = [i for i in range(20, n + 1)]
+print(list(filter(lambda x: not x % 20 or not x % 21, list_input)))
