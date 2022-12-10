b34251f77456bc9b9971efc848157ffedcb3e9bc
Alexander Konukhov <jesusmisha@gmail.com>
2022-12-10 17:05:53 +0300

    ex003.py done

--- python_gb/home_work/hw6/ex003.py
+++ python_gb/home_work/hw6/ex003.py
@@ -9,3 +9,12 @@
 # {'А': ['Алина'], 'Б': ['Бибочка'], 'И': ['Иван', 'Илья'],
 # 'М': ['Марина', 'Мария'], 'П': ['Петр', 'Петр']}
 #
+
+
+dict_of_employees = {}
+for name in iter(input, ''):
+    if name[0].upper() in dict_of_employees:
+        dict_of_employees[name[0].upper()].append(name)
+    else:
+        dict_of_employees[name[0].upper()] = [name]
+print(dict(sorted(dict_of_employees.items())))
