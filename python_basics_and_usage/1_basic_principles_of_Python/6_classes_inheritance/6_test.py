902012a4cd7673210fc9230d9082be99c3c88a09
Alexander Konukhov <jesusmisha@gmail.com>
2022-12-28 11:34:52 +0300

    added tests

---
+++ python_basics_and_usage/1_basic_principles_of_Python/6_classes_inheritance/6_test.py
@@ -0,0 +1,26 @@
+# Рассмотрим следующее объявление классов
+class A:
+    pass
+
+
+class B(A):
+    pass
+
+
+class C:
+    pass
+
+
+class D(C):
+    pass
+
+
+class E(B, C, D):
+    pass
+
+# Какие последовательности могут являться корректным порядком разрешения методов для класса E?
+# E, B, C, D, A, object
+# E, B, A, C, D, object
+# E, B, D, C, A, object
+# E, B, A, D, C, object
+# Никакие из перечисленных <- correct
