46fd5259e998dc48c4ef7d6a72520e47d7024e0f
Alexandr Konukhov <jesusmisha@gmail.com>
2023-01-07 11:00:28 +0300

    part import, __all__

--- python_basics_and_usage/2_standart_python_features/2_modules/for_import.py
+++ python_basics_and_usage/2_standart_python_features/2_modules/for_import.py
@@ -1,10 +1,18 @@
+GREETING = "Hello, "
+_SECOND_GREETING = "Hello, "
+
+
 class BadName(Exception):
     pass
 
 
 def greet(name):
     if name[0].isupper():
-        return "Hello, %s" % name
+        return _SECOND_GREETING + name
     else:
         raise BadName("Inappropriate name, %s" % name)
+
+
 print("&*()")
+
+#__all__ = ["BadName", "greet"]
