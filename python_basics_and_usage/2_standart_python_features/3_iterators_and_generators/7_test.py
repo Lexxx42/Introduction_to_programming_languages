9e23d4238b490a299643a02b2aff40e4e33a8012
Alexandr Konukhov <jesusmisha@gmail.com>
2023-01-09 13:17:54 +0300

    test

---
+++ python_basics_and_usage/2_standart_python_features/3_iterators_and_generators/7_test.py
@@ -0,0 +1,10 @@
+# Отметьте верные способы создать лист с элементами [1, 2, 3, 4]
+
+
+# a = [i + 1 for i in range(4)] <-
+
+# a = [i for i in range(4)] bad!
+
+# a = [i for i in range(5)][1:] <-
+
+# a = list(i + 1 for i in range(4)) <-
