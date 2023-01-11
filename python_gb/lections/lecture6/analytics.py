067cf403560442454c13c3801a47cfeed3a5610a
Alexander Konukhov <jesusmisha@gmail.com>
2023-01-11 20:48:47 +0300

    6

--- python_gb/lections/lecture6/analytics.py
+++ python_gb/lections/lecture6/analytics.py
@@ -4,11 +4,11 @@ from sympy.plotting import plot
 x = Symbol('x')
 y = Symbol('y')
 
-f = 21*x + 56*y - 1034
+f = 21 * x + 56 * y - 1034
 
 print(solve(f, x, y))
 
-f = x**2
+f = x ** 2
 
 plot(f, x)
 print(f)
