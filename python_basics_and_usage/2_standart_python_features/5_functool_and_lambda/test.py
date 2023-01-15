c0783e5b8437125aed667e9cb3c5dc56e7b9cd51
Alexander Konukhov <jesusmisha@gmail.com>
2023-01-15 13:46:32 +0300

    lambda

---
+++ python_basics_and_usage/2_standart_python_features/5_functool_and_lambda/test.py
@@ -0,0 +1,16 @@
+x = [
+    ("Guido", 1),
+    ("Heskel", 12),
+    ("John", 34)
+]
+
+
+def sorting(name):
+    return name[1]
+
+
+# name_lengths = [sorting(name) for name in x]
+# print(name_lengths)
+
+x.sort(key=lambda x: x[1], reverse=True)
+print(x)
