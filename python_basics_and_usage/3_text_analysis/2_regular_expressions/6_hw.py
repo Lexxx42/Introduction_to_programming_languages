8d5b54eb4c7bcdb33e8809b9a0869d9eb461e923
Alexandr Konukhov <jesusmisha@gmail.com>
2023-01-23 12:10:50 +0300

    hw6

---
+++ python_basics_and_usage/3_text_analysis/2_regular_expressions/6_hw.py
@@ -0,0 +1,34 @@
+# Вам дана последовательность строк.
+# Выведите строки, содержащие "cat" в качестве подстроки хотя бы два раза.
+#
+# Примечание:
+# Считать все строки по одной из стандартного потока ввода вы можете, например, так
+#
+# import sys
+#
+# for line in sys.stdin:
+#     line = line.rstrip()
+#     # process line
+#
+# Sample Input:
+#
+# catcat
+# cat and cat
+# catac
+# cat
+# ccaatt
+# Sample Output:
+#
+# catcat
+# cat and cat
+
+
+import sys
+import re
+
+sys.stdin = open("hw6.txt")
+pattern = "cat"
+
+for line in sys.stdin:
+    line = line.strip()
+    print(line + '\n' if len(re.findall(pattern, line)) >= 2 else "", end='')
