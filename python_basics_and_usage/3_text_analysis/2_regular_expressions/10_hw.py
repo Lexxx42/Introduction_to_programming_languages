dbd643c91fde463c41cb61e24909c847bda6826f
Alexandr Konukhov <jesusmisha@gmail.com>
2023-01-23 16:48:20 +0300

    hw10

---
+++ python_basics_and_usage/3_text_analysis/2_regular_expressions/10_hw.py
@@ -0,0 +1,25 @@
+# Вам дана последовательность строк.
+# Выведите строки, содержащие слово, состоящее из двух одинаковых частей (тандемный повтор).
+#
+# Sample Input:
+#
+# blabla is a tandem repetition
+# 123123 is good too
+# go go
+# aaa
+# Sample Output:
+#
+# blabla is a tandem repetition
+# 123123 is good too
+
+import sys
+import re
+
+pattern = r""
+sys.stdin = open("hw10.txt")
+for line in sys.stdin:
+    line = line.strip()
+    if re.search(pattern, line):
+        print(line)
+
+# https://habr.com/ru/post/349860/
