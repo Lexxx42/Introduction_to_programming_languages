3d19e2df9b81c32a19542db3699d51afaeb441a9
Alexander Konukhov <jesusmisha@gmail.com>
2023-01-27 21:21:44 +0300

    not working

--- python_basics_and_usage/3_text_analysis/4_csv_json/hw1.py
+++ python_basics_and_usage/3_text_analysis/4_csv_json/hw1.py
@@ -46,17 +46,19 @@ data_read = json.loads(file)
 dict_json = {}
 
 for i, item in enumerate(data_read):
-    if not data_read[i]["parents"]:
-        dict_json[data_read[i]["name"]] = set(data_read[i]["name"])
-        continue
     for parent in data_read[i]["parents"]:
+        a = []
         if parent not in dict_json:
             dict_json[parent] = set(data_read[i]["name"])
         else:
             dict_json.get(parent).add(data_read[i]["name"])
-sorted_tuples = sorted(dict_json.items(), key=lambda item: item[1])
+sorted_tuples = sorted(dict_json.items(), key=lambda item: item[0])
 dict_json = {k: v for k, v in sorted_tuples}
+
+for key, value in dict_json.items():
+    dict_json[key] = set(sorted(value))
 print(dict_json)  # parents:children
+
 list_1 = []
 
 children = set()
@@ -69,8 +71,16 @@ print(children)
 data2 = {i: 0 for i in children}
 print(data2)  # РЕБЕНОК : КОЛ-ВО ПОТОМКОВ
 
-for i in dict_json:
-    visited = []
-    f()
-
 # Для тех, кто отчаялся : из введенного текста json создаем первый словарь data( значение name становится ключом, а значение parents становится его значением), который имеет вид РЕБЕНОК : [РОДИТЕЛИ], второй словарь  data2 имеет вид РЕБЕНОК : КОЛ-ВО ПОТОМКОВ. Изначально значения ключей в data2 равны нулю. Запускаем цикл для data, и в нем будет создаваться пустой список visited и вызываться функция, в которую передаем ключ и его значение(РЕБЕНОК, [РОДИТЕЛИ]). В функции запускаем цикл для [РОДИТЕЛИ] , делаем в нем проверку, есть ли вершина в списке visited( вершина в данном случае - это переменная цикла). Если нету, то к значению data2[вершина] прибавляем 1(тут использовать конструкцию try..except, т.к. не факт, что данный класс объявлялся, и если ловим исключение то создаем ключ для данного класса в data2 со значением 1). Затем добавляем эту вершину в список visited и снова вызываем функцию(РЕБЕНОК, data[вершина]).  В конце просто вывести ключи со значениями data2 по алфавиту. Для тех, кто решил, можете посмотреть решение #27011572. Удачи)
+
+# def dfs(graph, start):
+#     visited, stack = set(), [start]
+#     while stack:
+#         vertex = stack.pop()
+#         if vertex not in visited:
+#             visited.add(vertex)
+#             stack.extend(graph[vertex] - visited)
+#     return visited
+#
+#
+# print(dfs(dict_json, "A"))
