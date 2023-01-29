f811a43b4aa84341226068d17724fcbd032ed381
Alexander Konukhov <jesusmisha@gmail.com>
2023-01-29 15:09:44 +0300

    done

--- python_basics_and_usage/3_text_analysis/4_csv_json/hw1.py
+++ python_basics_and_usage/3_text_analysis/4_csv_json/hw1.py
@@ -34,62 +34,8 @@
 # A : 3
 # B : 1
 # C : 2
+
 import json
-import networkx as nx
-from itertools import count
-
-# Ответ:
-#
-# A : 5
-# B : 1
-# C : 4
-# D : 2
-# E : 1
-# F : 3
-
-# input_json = [{"name": "A", "parents": []}, {"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}]
-input_json = [{"name": "beta", "parents": ["alpha", "gamma"]}, {"name": "gamma", "parents": ["alpha"]},
-              {"name": "alpha", "parents": []}, {"name": "delta", "parents": ["gamma", "zeta"]},
-              {"name": "epsilon", "parents": ["delta"]}, {"name": "zeta", "parents": []}]
-# input_json = json.loads(input())
-
-file = json.dumps(input_json)
-data_read = json.loads(file)
-
-dict_json = {}
-
-for i, item in enumerate(data_read):
-    for parent in item["parents"]:
-        if parent not in dict_json:
-            dict_json[parent] = {item["name"]}
-        else:
-            dict_json.get(parent).add(item["name"])
-sorted_tuples = sorted(dict_json.items(), key=lambda item: item[0])
-dict_json = {k: v for k, v in sorted_tuples}
-
-for key, value in dict_json.items():
-    dict_json[key] = set(sorted(value))
-print("parents:children", dict_json)  # parents:children
-
-list_1 = []
-
-children = set()
-for j, item in enumerate(data_read):
-    children.add(item['name'])
-children = sorted(children)
-
-print(children)
-
-data2 = {i: 0 for i in children}
-print(data2)  # РЕБЕНОК : КОЛ-ВО ПОТОМКОВ
-
-# for key in data2:
-#     print(f"{key} : {(len(dict_json[key])+1) if key in dict_json else 1}")
-
-dict_json_children = {}
-for i, item in enumerate(data_read):
-    dict_json_children[item["name"]] = set(sorted((data_read[i]["parents"])))
-print("children:parents", dict_json_children)
 
 
 def dfs(graph, start):
@@ -102,15 +48,30 @@ def dfs(graph, start):
     return visited
 
 
+# input_json = [{"name": "A", "parents": []}, {"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}]
+input_json = [{"name": "A", "parents": []}, {"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}]
+# input_json = json.loads(input())
+
+file = json.dumps(input_json)
+data_read = json.loads(file)
+
+dict_json_children = {}
+for i, item in enumerate(data_read):
+    dict_json_children[item["name"]] = set(sorted((data_read[i]["parents"])))
+
+children = set()
+for j, item in enumerate(data_read):
+    children.add(item['name'])
+children = sorted(children)
+list_1 = []
 
 for child in children:
     list_1.append(dfs(dict_json_children, child))
-print(list_1)
+
 list_2 = []
 for i, item in enumerate(list_1):
-    print(item)
     for elem in item:
         list_2.append(elem)
-print(sorted(list_2))
+
 for child in children:
     print(f"{child} : {list_2.count(child)}")
