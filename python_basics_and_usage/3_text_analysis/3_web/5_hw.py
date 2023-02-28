04cf17f4c436f8f0e18c3b0dd8401687e7c26cd9
Alexander Konukhov <jesusmisha@gmail.com>
2023-02-28 23:53:31 +0300

    done

--- python_basics_and_usage/3_text_analysis/3_web/5_hw.py
+++ python_basics_and_usage/3_text_analysis/3_web/5_hw.py
@@ -27,66 +27,13 @@
 # www.ya.ru
 # ya.ru
 
-# import requests
-# import lxml.html
-import tldextract
-
-#
-# list_links = set()
-# link = requests.get(input().strip().replace('stepic.org', 'stepik.org'))
-# html_1 = lxml.html.document_fromstring(link.text)
-#
-# for a in html_1.iter('a'):
-#     list_links.add(a.get('href'))
-# list_links.remove(None)
-#
-# answer = set()
-# for link in list_links:
-#     ext = tldextract.extract(link)
-#     if ext.domain:
-#         answer.add('.'.join(ext) if ext.subdomain else '.'.join(ext[1:]))
-#
-# for item in sorted(answer):
-#     print(item)
-
-
-# import requests
-# import re
-#
-# #link = input().strip().replace('stepic.org', 'stepik.org')
-# #file = requests.get(link).text
-file = r'''<a href="https://stepic.org/media/attachments/lesson/24472/sample1.html">1</a>
-<a href="http://stepic.org/courses">
-<a href='https://stepic.org'>
-<a href='http://neerc.ifmo.ru:1345'>
-<a href="ftp://mail.ru/distib">
-<a href="ya.ru">
-<a href="www.ya.ru">
-<a href="../skip_relative_links">
-<a href="../some_path/index.html">
-<a href="sas-_0123d.ifmo.ru">
-<a target='_top' href="http://redir.rbc.ru/cgi-bin/redirect.cgi?http://hc.ru/ru/">'''
-# print(*sorted(set(re.findall(r'<a[^>]* href=[\"\'](\w*w*.?\w+.\w+)[\"\']', file))), sep='\n')
-#
-# # a = set(re.findall(r'<a[^>]* href="([^"\']*)"', file))
-# # b = set()
-# # for item in a:
-# #     b.add((li if len(li := re.findall(r'\S+://([^/]+)/\S+', item)) == 1 else ["+"])[0])
-# # b.remove("+")
-# # for item in sorted(b):
-# #     print(item)
-
+# 'http://pastebin.com/raw/2mie4QYa'
+link = 'http://pastebin.com/raw/hfMThaGb'
 import requests
 import re
 
-# link = input().strip().replace('stepic.org', 'stepik.org')
+file = requests.get(link.strip()).text
+a = set(re.findall(r'(?:<a.+?href.*?=.*?[\'\"])(?:.+?://)?(\w[\w\-\.]+)', file))
 
-# file = requests.get(link).text
-a = set(re.findall(r'<a[^>]* href="([^"\']*)"', file))
-print(a)
-b = set()
-for item in a:
-    b.add((li if len(li := re.findall(r'', item)) == 1 else ["+"])[0])
-b.remove("+")
-for item in sorted(b):
+for item in sorted(a):
     print(item)
